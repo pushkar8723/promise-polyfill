95cee2ad2fc66bc38f30bc842f57b88849529b41
pushkar8723 <pushkar8723@gmail.com>
2024-09-11 20:31:20 +0530

    clean up

---
+++ src/main.js
@@ -0,0 +1,29 @@
+import './styles/style.css';
+import MyPromise from './ActualPromise';
+
+const p = new MyPromise((resolve) => {
+  resolve('Hello then');
+});
+
+p.then((data) => {
+  console.log(data);
+  return MyPromise.reject(`${data} reject`);
+})
+  .then((data) => {
+    console.log('again', data);
+    return data;
+  })
+  .finally(() => {
+    console.log('finally 1');
+  })
+  .then((data) => console.log(`again again ${data}`))
+  .catch((err) => console.log(err))
+  .finally(() => {
+    console.log('finally 2');
+  });
+
+document.querySelector('#app').innerHTML = `
+  Check browser console
+`;
+
+setupCounter(document.querySelector('#counter'));
